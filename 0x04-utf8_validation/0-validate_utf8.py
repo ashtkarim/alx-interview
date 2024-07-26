dce117b99a049fe5999ed916f69a80d295565e1b
ashtkarim <assihoutkarim@gmail.com>
2024-07-26 17:50:56 +0100

    utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    Args:
+        - data: A list of integers, each integer represents 1 byte of data
+    Returns:
+        - True if data is a valid UTF-8 encoding, otherwise False
+    """
+    byte_count = 0
+    for byte in data:
+        # Apply bit mask to get the 8 least significant bits
+        byte = byte & 0xFF
+
+        # Convert byte to binary and get the first 8 bits
+        byte_bin = format(byte, '08b')
+
+        if byte_count == 0:
+            # determine the number of bytes in the character
+            if (byte_bin.startswith('11110')):
+                byte_count = 3
+            elif (byte_bin.startswith('1110')):
+                byte_count = 2
+            elif (byte_bin.startswith('110')):
+                byte_count = 1
+            elif not byte_bin.startswith('0'):
+                return False
+        else:
+            # Check if byte is continuation byte (starts with '10')
+            if not byte_bin.startswith('10'):
+                return False
+            byte_count -= 1
+
+    return byte_count == 0
